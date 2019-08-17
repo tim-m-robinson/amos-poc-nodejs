a4a3539b8c90b9805b5048b3f2f14e4f7b5b395a
belphegor666 <the_luggage@bigfoot.com>
2019-08-17 08:26:13 +0100

    crypto

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 /*
  *  * Module dependencies
  *   */
+var crypto = require('crypto');
+
+var hash = crypto.createHash('sha256');
+
+var code = 'bacon';
+
 var express = require('express')
 
 var app = express()
 
 app.get('/', function (req, res) {
-   res.end('<html><body bgcolor=green><h1>Hi there!</h1></body></html>')
+  code = hash.update(code);
+  code = hash.digest(code);
+
+  msg = Buffer.from(code).toString('base64');
+  console.log(msg);
+
+console.log(code);
+  res.end('<html><body bgcolor=green><h1>Hi there! '+msg+'</h1></body></html>')
 })
 app.listen(8080)
