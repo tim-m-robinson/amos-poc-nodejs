474e723da0db4c0cb52c696d4ad42423855b5825
belphegor666 <the_luggage@bigfoot.com>
2019-08-17 08:12:56 +0100

    whitespace

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.end('<html><body bgcolor=green><h1>Hi there!</h1></body></html>')
+   res.end('<html><body bgcolor=green><h1>Hi there!</h1></body></html>')
 })
 app.listen(8080)
